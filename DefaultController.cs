d4424adc4d1e0ae6606af7cae65ddcb3355d8de9
abdelrahmanamr54 <111569311+abdelrahmanamr54@users.noreply.github.com>
2022-12-20 00:33:45 +0200

    Add files via upload

---
+++ DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            AdventureWorks2017Entities dc= new AdventureWorks2017Entities();
+            var items=dc.Addresses.ToList();
+            return View(items);
+        }
+    }
+}
